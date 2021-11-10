1b8f9ac9424e3748afccc5fd3085db50a2e44047
EC2 Default User <ec2-user@ip-172-31-34-61.ap-northeast-1.compute.internal>
2021-11-10 22:59:22 +0000

    [fix]画像投稿詳細画面修正及び Refile.secret_keyの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1c6643c3b56b297cb5a1c1b4e024628867a2adb18b193002b4e058e348053438fa8a134625b7e921492af3e2c62dc2277c4bd3d4a836f2591115fd5f182db60c'
